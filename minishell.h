2766640b22fd57f58297b264fd710d092313fff2
Karim Chaouki <kchaouki@c1r3p9.1337.ma>
2023-05-03 09:16:56 +0100

    start

--- minishell.h
+++ minishell.h
@@ -6,14 +6,17 @@
 /*   By: kchaouki <kchaouki@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 15:40:00 by kchaouki          #+#    #+#             */
-/*   Updated: 2023/05/02 15:40:01 by kchaouki         ###   ########.fr       */
+/*   Updated: 2023/05/03 08:47:24 by kchaouki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-
-
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
 # endif
--- test.c
+++ test.c
@@ -6,40 +6,19 @@
 /*   By: kchaouki <kchaouki@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 15:40:05 by kchaouki          #+#    #+#             */
-/*   Updated: 2023/05/02 15:40:06 by kchaouki         ###   ########.fr       */
+/*   Updated: 2023/05/03 08:46:49 by kchaouki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <stdio.h>
-// #include <unistd.h>
-// #include <readline/readline.h>
-// #include <readline/history.h>
-
-// int main()
-// {
-
-// 	char *read_line = readline("minishell > ");
-// 	return (0);
-// }
-
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <readline/readline.h>
-#include <readline/history.h>
+#include "minishell.h"
 
 int main()
 {
-	// printf("test\n");
 	char *read_line;
 	while (1)
 	{
-    	read_line = readline("minishell > ");
+    	read_line = readline("minishell> ");
 		add_history(read_line);
 	}
-    // write(1, "\n", 1);
-	// rl_on_new_line();
-	// rl_replace_line("", 0);
-	// rl_redisplay();
 	return 0;
 }
