8f055602ada4ddd6f46a47e0cc19781049f27945
Karim Chaouki <kchaouki@c2r9p6.1337.ma>
2023-05-31 18:50:34 +0100

    change env to {var:value}

--- parsing/environment.c
+++ parsing/environment.c
@@ -6,7 +6,7 @@
 /*   By: kchaouki <kchaouki@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 19:30:32 by rrhnizar          #+#    #+#             */
-/*   Updated: 2023/05/23 16:29:12 by kchaouki         ###   ########.fr       */
+/*   Updated: 2023/05/31 18:45:39 by kchaouki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,14 @@ void	free_env(t_env *env)
 	while (tmp)
 	{
 		tmp2 = tmp->next;
-		free (tmp->content);
+		free (tmp->var);
+		free (tmp->value);
 		free (tmp);
 		tmp = tmp2;
 	}
 }
 
-static void	add_to_env(t_env **env, char *content)
+static void	add_to_env(t_env **env, char *var, char *value)
 {
 	t_env	*new;
 	t_env	*tmp;
@@ -38,8 +39,10 @@ static void	add_to_env(t_env **env, char *content)
 		free_env(*env);
 		return ;
 	}
-	new->content = content;
+	new->var = var;
+	new->value = value;
 	new->next = NULL;
+	new->prev = NULL;
 	if (!*env)
 	{
 		*env = new;
@@ -49,6 +52,17 @@ static void	add_to_env(t_env **env, char *content)
 	while (tmp->next)
 		tmp = tmp->next;
 	tmp->next = new;
+	new->prev = tmp;
+}
+
+static int	find_equale(char *str)
+{
+	int count;
+
+	count = 0;
+	while (str[count] && str[count] != '=')
+		count++;
+	return (count);
 }
 
 t_env	*create_env(char **envp)
@@ -58,41 +72,43 @@ t_env	*create_env(char **envp)
 	env = NULL;
 	while (*envp)
 	{
-		add_to_env(&env, strdup(*envp));
+		add_to_env(&env, ft_strdup(ft_substr(*envp, 0, find_equale(*envp))), \
+		ft_strdup(ft_strchr(*envp, '=') + 1));
 		envp++;
 	}
 	return (env);
 }
 
-static int	count_env_variables(t_env *env)
-{
-	t_env	*tmp;
-	int		count;
 
-	count = 0;
-	tmp = env;
-	while (tmp)
-	{
-		count++;
-		tmp = tmp->next;
-	}
-	return (count);
-}
+// static int	count_env_variables(t_env *env)
+// {
+// 	t_env	*tmp;
+// 	int		count;
 
-char	**env_to_double_ptr(t_env *env)
-{
-	char	**output;
-	int		i;
+// 	count = 0;
+// 	tmp = env;
+// 	while (tmp)
+// 	{
+// 		count++;
+// 		tmp = tmp->next;
+// 	}
+// 	return (count);
+// }
 
-	output = malloc(sizeof(char *) * (count_env_variables(env) + 1));
-	if (!output)
-		return (NULL);
-	i = 0;
-	while (env)
-	{
-		output[i++] = ft_strdup(env->content);
-		env = env->next;
-	}
-	output[i] = NULL;
-	return (output);
-}
+// char	**env_to_double_ptr(t_env *env)
+// {
+// 	char	**output;
+// 	int		i;
+
+// 	output = malloc(sizeof(char *) * (count_env_variables(env) + 1));
+// 	if (!output)
+// 		return (NULL);
+// 	i = 0;
+// 	while (env)
+// 	{
+// 		output[i++] = ft_strdup(env->content);
+// 		env = env->next;
+// 	}
+// 	output[i] = NULL;
+// 	return (output);
+// }
